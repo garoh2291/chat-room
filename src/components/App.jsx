070f726eebf2614a229baa29981ca1c20fbcc3e2
Garnik Hovsepyan <garoh2291@gmail.com>
2023-01-06 00:59:56 +0400

    demo mode of chat

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { AppRoutes } from "./AppRoutes";
+import "../styles/main.css";
+
+export const App = () => {
+  return (
+    <div className="container">
+      <AppRoutes />
+    </div>
+  );
+};
---
+++ src/components/Chat.jsx
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+import { useLocation, useNavigate } from "react-router-dom";
+import EmojiPicker from "emoji-picker-react";
+
+import icon from "../images/emoji.svg";
+import styles from "../styles/Chat.module.css";
+import { Messages } from "./Messages";
+
+const socket = io.connect("http://localhost:5010");
+
+export const Chat = () => {
+  const [state, setState] = useState([]);
+  const { search } = useLocation();
+  const [params, setParams] = useState({ room: "", user: "" });
+  const [message, setMessage] = useState("");
+  const [isOpen, setOpen] = useState(false);
+  const [users, setUsers] = useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const searchParams = Object.fromEntries(new URLSearchParams(search));
+    setParams(searchParams);
+    socket.emit("join", searchParams);
+  }, [search]);
+
+  useEffect(() => {
+    socket.on("message", ({ data }) => {
+      setState((prev) => [...prev, data]);
+    });
+  }, []);
+
+  useEffect(() => {
+    socket.on("room", ({ data: { users } }) => {
+      setUsers(users.length);
+    });
+  }, []);
+
+  const leftRoom = () => {
+    socket.emit("leftRoom", { params });
+    navigate("/");
+  };
+
+  const handleChange = ({ target: { value } }) => {
+    setMessage(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!message) return;
+
+    socket.emit("sendMessage", { message, params });
+
+    setMessage("");
+  };
+
+  const onEmojiClick = ({ emoji }) => setMessage(`${message}${emoji} `);
+
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.header}>
+        <div className={styles.title}>{params.room}</div>
+        <div className={styles.users}>{users} users in this room</div>
+        <button className={styles.left} onClick={leftRoom}>
+          Left the room
+        </button>
+      </div>
+
+      <div className={styles.messages}>
+        <Messages messages={state} name={params.name} />
+      </div>
+
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.input}>
+          <input
+            type="text"
+            name="message"
+            value={message}
+            placeholder="What do You want to write???"
+            autoComplete="off"
+            required
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.emoji}>
+          <img src={icon} alt="emoji" onClick={() => setOpen(!isOpen)} />
+
+          {isOpen && (
+            <div className={styles.emojies}>
+              <EmojiPicker onEmojiClick={onEmojiClick} />
+            </div>
+          )}
+        </div>
+        <div className={styles.button}>
+          <input type="submit" onSubmit={handleSubmit} value="Send a message" />
+        </div>
+      </form>
+    </div>
+  );
+};
